3b79cd78adb4d86b869cc1b8eb487bf51f494a9a
Rizwan Patel <rpatel@ext.alohaaba.com>
2024-10-31 13:17:31 +0530

    added parent category name

--- sustainbean_api/Models/Category.cs
+++ sustainbean_api/Models/Category.cs
@@ -14,4 +14,19 @@
         public string? updated_by { get; set; } // updated_by (nullable)
     }
 
+    public class CategoryGrid
+    {
+        public int category_id { get; set; } // category_id
+        public string? category { get; set; } // category
+        public string? slug { get; set; } // slug
+        public string? parent_category { get; set; } // parent_category
+        public string? parent_category_name { get; set; } // parent_category
+        public string? description { get; set; } // description
+        public bool? is_active { get; set; } // is_active
+        public DateTime created_on { get; set; } // created_on
+        public string? created_by { get; set; } // created_by
+        public DateTime? updated_on { get; set; } // updated_on (nullable)
+        public string? updated_by { get; set; } // updated_by (nullable)
+    }
+
 }
